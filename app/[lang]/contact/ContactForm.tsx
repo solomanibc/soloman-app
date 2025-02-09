9f2fc3244de5be6c1f0796581a7147775572d6f3
faresG <guizanifares0@gmail.com>
2025-02-09 16:07:49 +0100

    1 feedback

--- app/[lang]/contact/ContactForm.tsx
+++ app/[lang]/contact/ContactForm.tsx
@@ -1,19 +1,18 @@
-import { type LocaleParams, getDictionary } from "@/app/[lang]/dictionaries";
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 
-export default async function ContactForm({ lang }: LocaleParams) {
-	const dict = await getDictionary(lang);
-
+export default function ContactForm() {
 	return (
 		<div id="contact-form" className="py-10">
 			<form className="space-y-6">
-				<Input placeholder={dict.contact.namePlaceholder} />
-				<Input type="email" placeholder={dict.contact.emailPlaceholder} />
-				<Textarea placeholder={dict.contact.messagePlaceholder} />
+				<Input placeholder="Name" />
+				<Input type="email" placeholder="Email" />
+				<Textarea placeholder="Message" />
 				<Button type="submit" className="w-full bg-primary hover:bg-primary/80">
-					{dict.contact.submit}
+					Submit
 				</Button>
 			</form>
 		</div>

405f6d1441fddc22d1c11aa9034c521930055602
Fahari <mkl.devops@gmail.com>
2025-07-09 08:52:37 +0100

    feat: mise à jour du composant Partnership avec un nouveau logo UK PIN et amélioration de la structure

--- app/[lang]/Footer/Partnership.tsx
+++ app/[lang]/Footer/Partnership.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
 
-import { env } from "@/lib/env";
 import type { Dictionary } from "@/types/dictionnary.type";
 
 export function Partnership({ dict }: { dict: Dictionary }) {
@@ -9,17 +8,15 @@ export function Partnership({ dict }: { dict: Dictionary }) {
 		<div>
 			<h4 className="text-lg font-semibold mb-4">{dict.footer.partnerships}</h4>
 			<div className="flex items-center space-x-4 justify-center">
-				{env.ENABLE_PARTNERSHIPS && (
-					<Link href="https://www.pin.uk.com/" target="_blank">
-						<Image
-							alt="PIN UK Logo"
-							className="object-contain"
-							height={40}
-							src="/icons/pin-uk.png"
-							width={80}
-						/>
-					</Link>
-				)}
+				<Link href="https://www.ukpin.com/" target="_blank">
+					<Image
+						alt="UK PIN Logo"
+						className="object-contain"
+						height={40}
+						src="/icons/uk-pin.png"
+						width={80}
+					/>
+				</Link>
 
 				<Link href="https://hk-pi.org.hk/" target="_blank">
 					<Image
